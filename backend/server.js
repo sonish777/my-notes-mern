4369139321a974d949ea1402654a1d02cd67a3a2
Sonish Maharjan <sonishmaharjan1@gmail.com>
2020-11-20 21:05:44 +0545

    changes

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // app.use("/directories", directoryRoutes);
 app.use("/semesters", semesterRoutes);
 app.use("/subjects", subjectRoutes);
